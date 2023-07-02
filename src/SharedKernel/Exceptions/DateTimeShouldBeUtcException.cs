e88860aa7f7e71be36d900cbfce70179735f78c7
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2023-07-02 18:38:29 -0300

    refact: remove unnecessary constructors

--- src/SharedKernel/Exceptions/DateTimeShouldBeUtcException.cs
+++ src/SharedKernel/Exceptions/DateTimeShouldBeUtcException.cs
@@ -6,16 +6,5 @@ namespace ShopDemo.SharedKernel.Exceptions;
 public class DateTimeShouldBeUtcException
     : Exception
 {
-    /*
-     * This constructors implement sonar rule id csharpsquid:S3925
-     * The objective of this rule is make serializations constructors 
-     * explicit for programmer not forget of programming the 
-     * serialization process if necessary
-     */
 
-    // Constructors
-    public DateTimeShouldBeUtcException() { }
-    public DateTimeShouldBeUtcException(string message) : base(message) { }
-    public DateTimeShouldBeUtcException(string message, Exception innerException) : base(message, innerException) { }
-    protected DateTimeShouldBeUtcException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 }
