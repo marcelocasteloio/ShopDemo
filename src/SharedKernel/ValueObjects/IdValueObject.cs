4499d1d159c9fd99f976cd547095cea0ac2b259a
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2023-07-02 00:31:00 -0300

    feat: add idValueObject

---
+++ src/SharedKernel/ValueObjects/IdValueObject.cs
@@ -0,0 +1,48 @@
+﻿using MassTransit.NewIdProviders;
+using MassTransit;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ShopDemo.SharedKernel.ValueObjects;
+public readonly struct IdValueObject
+{
+    // Properties
+    public Guid Value { get; }
+
+    // Constructors
+    static IdValueObject()
+    {
+        /*
+         * Add current process id to guid generation process
+         * to avoid problemns with multithread id generations
+         */
+        NewId.SetProcessIdProvider(new CurrentProcessIdProvider());
+    }
+    private IdValueObject(Guid value)
+    {
+        Value = value;
+    }
+    private IdValueObject(IdValueObject value)
+    {
+        Value = value.Value;
+    }
+
+    // Operators
+    public static implicit operator IdValueObject(Guid value) => new(value);
+    public static implicit operator Guid(IdValueObject value) => value.Value;
+
+    public static bool operator ==(IdValueObject left, IdValueObject right) => left.Value == right.Value;
+    public static bool operator !=(IdValueObject left, IdValueObject right) => !(left.Value == right.Value);
+    public static bool operator >(IdValueObject left, IdValueObject right) =>  left.Value > right.Value;
+    public static bool operator >=(IdValueObject left, IdValueObject right) =>  left.Value >= right.Value;
+    public static bool operator <(IdValueObject left, IdValueObject right) =>  left.Value < right.Value;
+    public static bool operator <=(IdValueObject left, IdValueObject right) =>  left.Value <= right.Value;
+
+    // Public Methods
+    public override bool Equals([NotNullWhen(true)] object? obj) => obj is not null && this == (IdValueObject)obj;
+    public override int GetHashCode() => Value.GetHashCode();
+
+    // Builders
+    public static IdValueObject GenerateNewId() => NewId.NextGuid();
+    public static IdValueObject FromExistingId(Guid id) => new IdValueObject(id).Value;
+    public static IdValueObject FromExistingId(IdValueObject id) => new IdValueObject(id).Value;
+}
---
+++ tst/Tests.UnitTests/SharedKernelTests/ValueObjectsTests/IdValueObjectTests.cs
@@ -0,0 +1,220 @@
+﻿using FluentAssertions;
+using ShopDemo.SharedKernel.ValueObjects;
+using System.ComponentModel;
+
+namespace ShopDemo.Tests.UnitTests.SharedKernelTests.ValueObjectsTests;
+public class IdValueObjectTests
+{
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Generate_New_Id()
+    {
+        // Arrange and Act
+        var id = IdValueObject.GenerateNewId();
+
+        // Assert
+        id.Value.Should().NotBe(default(Guid));
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Generate_New_Sequential_Id()
+    {
+        // Arrange
+        var previousId = IdValueObject.GenerateNewId();
+
+        // Act
+        var nextId = IdValueObject.GenerateNewId();
+
+        // Assert
+        (nextId > previousId).Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Generate_From_Existing_Id()
+    {
+        // Arrange
+        var existringId = Guid.NewGuid();
+
+        // Act
+        var id = IdValueObject.FromExistingId(existringId);
+
+        // Assert
+        id.Value.Should().Be(existringId);
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Generate_From_Existing_IdValueObject()
+    {
+        // Arrange
+        var existringId = IdValueObject.GenerateNewId();
+
+        // Act
+        var id = IdValueObject.FromExistingId(existringId);
+
+        // Assert
+        id.Value.Should().Be(existringId);
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Implict_Converted_From_Guid()
+    {
+        // Arrange
+        var existringId = Guid.NewGuid();
+
+        // Act
+        IdValueObject id = existringId;
+
+        // Assert
+        id.Value.Should().Be(existringId);
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Implict_Converted_To_Guid()
+    {
+        // Arrange
+        var existringId = IdValueObject.GenerateNewId();
+
+        // Act
+        Guid id = existringId;
+
+        // Assert
+        id.Should().Be(existringId.Value);
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Use_Equals_Method()
+    {
+        // Arrange
+        var idA = IdValueObject.GenerateNewId();
+        var idB = idA;
+        var idC = IdValueObject.GenerateNewId();
+
+        // Act
+        var resultA = idA.Equals(idB);
+        var resultB = idA.Equals(idC);
+
+        // Assert
+        resultA.Should().BeTrue();
+        resultB.Should().BeFalse();
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Use_Equals_Operator()
+    {
+        // Arrange
+        var idA = IdValueObject.GenerateNewId();
+        var idB = idA;
+        var idC = IdValueObject.GenerateNewId();
+
+        // Act
+        var resultA = idA == idB;
+        var resultB = idA == idC;
+
+        // Assert
+        resultA.Should().BeTrue();
+        resultB.Should().BeFalse();
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Use_NotEquals_Operator()
+    {
+        // Arrange
+        var idA = IdValueObject.GenerateNewId();
+        var idB = idA;
+        var idC = IdValueObject.GenerateNewId();
+
+        // Act
+        var resultA = idA != idB;
+        var resultB = idA != idC;
+
+        // Assert
+        resultA.Should().BeFalse();
+        resultB.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Use_Greater_Than_Operator()
+    {
+        // Arrange
+        var idA = IdValueObject.GenerateNewId();
+        var idB = idA;
+        var idC = IdValueObject.GenerateNewId();
+
+        // Act
+        var resultA = idA > idB;
+        var resultB = idC > idA;
+
+        // Assert
+        resultA.Should().BeFalse();
+        resultB.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Use_Greater_Than_Or_Equals_Operator()
+    {
+        // Arrange
+        var idA = IdValueObject.GenerateNewId();
+        var idB = IdValueObject.GenerateNewId();
+        var idC = idB;
+        var idD = IdValueObject.GenerateNewId();
+
+        // Act
+        var resultA = idB >= idA;
+        var resultB = idB >= idC;
+        var resultC = idA >= idD;
+
+        // Assert
+        resultA.Should().BeTrue();
+        resultB.Should().BeTrue();
+        resultC.Should().BeFalse();
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Use_Less_Than_Operator()
+    {
+        // Arrange
+        var idA = IdValueObject.GenerateNewId();
+        var idB = idA;
+        var idC = IdValueObject.GenerateNewId();
+
+        // Act
+        var resultA = idB < idA;
+        var resultB = idA < idC;
+
+        // Assert
+        resultA.Should().BeFalse();
+        resultB.Should().BeTrue();
+    }
+
+    [Fact]
+    [Trait("SharedKernel", nameof(IdValueObject))]
+    public void IdValueObject_Should_Use_Less_Than_Or_Equals_Operator()
+    {
+        // Arrange
+        var idA = IdValueObject.GenerateNewId();
+        var idB = IdValueObject.GenerateNewId();
+        var idC = idB;
+        var idD = IdValueObject.GenerateNewId();
+
+        // Act
+        var resultA = idA <= idB;
+        var resultB = idC <= idB;
+        var resultC = idD <= idA;
+
+        // Assert
+        resultA.Should().BeTrue();
+        resultB.Should().BeTrue();
+        resultC.Should().BeFalse();
+    }
+}
