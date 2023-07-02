e2c329a80de48d6c4cf02aa39c0529f56ac44c29
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2023-07-02 02:45:58 -0300

    feat: add serialization code to DateTimeShouldBeUtcException

--- src/SharedKernel/Exceptions/DateTimeShouldBeUtcException.cs
+++ src/SharedKernel/Exceptions/DateTimeShouldBeUtcException.cs
@@ -1,7 +1,19 @@
 ﻿namespace ShopDemo.SharedKernel.Exceptions;
+
 [Serializable]
 public class DateTimeShouldBeUtcException
     : Exception
 {
-
+    // Constructors
+    protected DateTimeShouldBeUtcException(
+        System.Runtime.Serialization.SerializationInfo serializationInfo, 
+        System.Runtime.Serialization.StreamingContext streamingContext
+    )
+    {
+        throw new NotImplementedException();
+    }
+    public DateTimeShouldBeUtcException()
+    {
+        
+    }
 }
