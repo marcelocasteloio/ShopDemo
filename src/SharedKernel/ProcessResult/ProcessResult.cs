461daf9449806257631ac181e1020bdf948af2b5
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2023-07-13 02:05:17 -0300

    code smell: add non null check

--- src/SharedKernel/ProcessResult/ProcessResult.cs
+++ src/SharedKernel/ProcessResult/ProcessResult.cs
@@ -97,7 +97,7 @@ public readonly struct ProcessResult
             Array.Copy(
                 sourceArray: processResult.MessageCollection,
                 sourceIndex: 0,
-                destinationArray: newMessageArray,
+                destinationArray: newMessageArray!,
                 destinationIndex: lastIndex,
                 length: processResult.MessageCollection.Length
             );
